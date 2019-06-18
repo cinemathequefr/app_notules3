52d4beca01506d657af323833ba0fa116941b914
nltesown <nlte@magic.fr>
2019-06-18 15:45:23 +0200

    Add render script (adapted from v2)

---
+++ render.js
@@ -0,0 +1,110 @@
+const fs = require("fs");
+const _ = require("lodash");
+const {
+  promisify
+} = require("util");
+const helpers = require("./lib/helpers.js");
+const format = require("./lib/format.js");
+const doMerge = require("./lib/transforms/merge.js");
+const cleanTitreEvenement = require("./lib/transforms/clean_titre_evenement.js");
+const doRender = require("./lib/transforms/render.js");
+const doMarkdown = require("./lib/transforms/markdown.js");
+const doTaggedText = require("./lib/transforms/tt.js");
+const config = require("./lib/config.js");
+
+try {
+  let args = helpers.extractArgsValue(process.argv.slice(2).join(" "));
+  var idProg = helpers.toNumOrNull(args.p[0]);
+  var idCycle = helpers.toNumOrNull(args.c[0]);
+} catch (e) {
+  console.error("Erreur d'arguments. Les arguments attendus sont de la forme : -p <id programme> -c <id cycle>")
+}
+
+(async function () {
+  let progConfig = await helpers.fetchProgConfig(idProg);
+  let cycleConfig = helpers.cycleConfig(progConfig, idCycle);
+  let progDirectoryName = helpers.getFullCode.prog(progConfig).join(" "); // Nom du répertoire du programme
+  let cycleFullCode = helpers.getFullCode.cycle(progConfig, idCycle);
+
+  let films;
+  let seances;
+  let merge;
+  let render;
+  let markdown;
+  let taggedText;
+  let isDef = false;
+
+  // Lecture des données séances
+  seances = await helpers.readFileAsJson(
+    `${config.pathData.local}${progDirectoryName}/${cycleFullCode[0]}_SEANCES ${
+    cycleFullCode[1]
+  }.json`
+  );
+
+  // Lecture des données films
+  // On cherche en priorité le fichier _FILMS_DEF.json sur distant
+  // A défaut, on prend le fichier _FILMS.json sur local (données non définitives)
+  try {
+    films = await helpers.readFileAsJson(
+      `${config.pathData.remote}${progDirectoryName}/${cycleFullCode[0]} ${cycleFullCode[1]}/${cycleFullCode[0]}_FILMS_DEF ${
+        cycleFullCode[1]
+      }.json`
+    );
+    isDef = true;
+  } catch (e) {
+    try {
+      films = await helpers.readFileAsJson(
+        `${config.pathData.local}${progDirectoryName}/${cycleFullCode[0]}_FILMS ${
+          cycleFullCode[1]
+        }.json`
+      );
+      isDef = false;
+    } catch (e) {
+      console.log(e); // Erreur fatale : ni _FILMS_DEF.json, ni _FILMS.json n'ont été trouvés
+    }
+  }
+
+  // _MERGE : Fusion des données films et séances
+  merge = doMerge(cycleConfig, films, seances);
+  merge = cleanTitreEvenement(merge);
+  await helpers.writeFileInFolder(
+    `${config.pathData.local}${progDirectoryName}`,
+    "",
+    `${cycleFullCode[0]}_MERGE${isDef ? "_DEF" : ""} ${cycleFullCode[1]}.json`,
+    JSON.stringify(merge, null, 2),
+    "utf8"
+  );
+
+  // _RENDER : Transformation de _MERGE en prenant en compte les regroupements et tri dans les sous-cycles
+  render = {
+    header: cycleConfig,
+    data: doRender(merge.data)
+  };
+  await helpers.writeFileInFolder(
+    `${config.pathData.local}${progDirectoryName}`,
+    "",
+    `${cycleFullCode[0]}_RENDER${isDef ? "_DEF" : ""} ${cycleFullCode[1]}.json`,
+    JSON.stringify(render, null, 2),
+    "utf8"
+  );
+
+  // Conversion de _RENDER au format Markdown
+  markdown = doMarkdown(render);
+  await helpers.writeFileInFolder(
+    `${config.pathData.remote}${progDirectoryName}`,
+    `${cycleFullCode[0]} ${cycleFullCode[1]}`,
+    `${cycleFullCode[0]}_CYCLE${isDef ? "_DEF" : ""} ${cycleFullCode[1]}.md`,
+    markdown,
+    "utf8"
+  );
+
+  // Conversion de _RENDER au format Tagged Text
+  taggedText = doTaggedText(render);
+  await helpers.writeFileInFolder(
+    `${config.pathData.remote}${progDirectoryName}`,
+    `${cycleFullCode[0]} ${cycleFullCode[1]}`,
+    `${cycleFullCode[0]}_CYCLE${isDef ? "_DEF" : ""} ${cycleFullCode[1]}.txt`,
+    taggedText,
+    "latin1"
+  );
+})();
