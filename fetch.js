73ac69edc691b9370097bc2d1b6861d505d8dcdd
nltesown <nlte@magic.fr>
2019-11-19 17:44:30 +0100

    Ecrit un fichier distant _FILMS_ADD.json en cas de films supplementaires trouves.

--- fetch.js
+++ fetch.js
@@ -12,9 +12,7 @@ const confs = require("./lib/query/confs.js");
 const texts = require("./lib/query/texts.js");
 
 const helpers = require("./lib/helpers.js");
-const {
-  promisify
-} = require("util"); // https://stackoverflow.com/questions/40593875/using-filesystem-in-node-js-with-async-await
+const { promisify } = require("util"); // https://stackoverflow.com/questions/40593875/using-filesystem-in-node-js-with-async-await
 
 // const copyFile = promisify(fs.copyFile);
 
@@ -28,25 +26,26 @@ try {
   var doSeances = !_.isUndefined(args.s);
   var doConfs = !_.isUndefined(args.a); // Flag `a` comme "action culturelle"
   var doTexts = !_.isUndefined(args.t); // Flag `a` comme "action culturelle"
-
-
 } catch (e) {
-  console.error("Erreur d'arguments. Les arguments attendus sont de la forme : -p <id programme> -c <id cycle>.")
+  console.error(
+    "Erreur d'arguments. Les arguments attendus sont de la forme : -p <id programme> -c <id cycle>."
+  );
 }
 
 // TODO: vérification de l'existence du répertoire du programme (sinon erreur et suggérer d'exécuter le script init)
 
-(async function () {
+(async function() {
   let progConfig = await helpers.fetchProgConfig(idProg);
   let cycleConfig = helpers.cycleConfig(progConfig, idCycle);
   let progDirectoryName = helpers.getFullCode.prog(progConfig).join(" "); // Nom du répertoire du programme
   let cycleFullCode = helpers.getFullCode.cycle(progConfig, idCycle);
 
   try {
-
     const db = await database.attach(config.db);
 
-    console.log(`Importation des données pour le cycle ${cycleFullCode.join(" ")}.`);
+    console.log(
+      `Importation des données pour le cycle ${cycleFullCode.join(" ")}.`
+    );
     console.log("Connecté à la base de données.");
 
     // Films
@@ -62,13 +61,36 @@ try {
         JSON.stringify(f, null, 2),
         "utf8"
       );
+
+      // 2019-11-18 : On compare ces données avec celles d'un éventuel fichier _FILM_DEF.json pour voir s'il y a des films supplémentaires.
+      // Si c'est le cas, un fichier _FILMS_ADD.json contenant les ajouts est écrit.
+      try {
+        let f_def = await helpers.readFileAsJson(
+          `${config.pathData.remote}${progDirectoryName}/${cycleFullCode[0]} ${cycleFullCode[1]}/${cycleFullCode[0]}_FILMS_DEF ${cycleFullCode[1]}.json`
+        );
+
+        let f_additions = _.differenceBy(f, f_def, "idFilm");
+
+        if (f_additions.length > 0) {
+          await helpers.writeFileInFolder(
+            `${config.pathData.remote}${progDirectoryName}`,
+            `${cycleFullCode[0]} ${cycleFullCode[1]}`,
+            `${cycleFullCode[0]}_FILMS_ADD ${cycleFullCode[1]}.json`,
+            JSON.stringify(f_additions, null, 2),
+            "utf8"
+          );
+          console.log(
+            `${f_additions.length} films supplémentaires ont été trouvés. Un fichier d'ajouts a été écrit : ${cycleFullCode[0]}_FILMS_ADD ${cycleFullCode[1]}.json`
+          );
+        }
+      } catch (e) {}
     }
 
     // Confs (flag -a)
     if (doConfs) {
       console.log(`Requête confs.`);
       let c = await confs(db, cycleConfig);
-      console.log(`Conférences : ${_.map(c).length} items.`)
+      console.log(`Conférences : ${_.map(c).length} items.`);
       await helpers.writeFileInFolder(
         `${config.pathData.local}${progDirectoryName}`,
         "",
@@ -82,7 +104,18 @@ try {
     if (doTexts) {
       console.log(`Requête textes.`);
       let t = await texts(db, cycleConfig);
-      console.log(`Textes : ${_(t).map(d => _(d).map().value()).flattenDeep().value().length} items.`)
+      console.log(
+        `Textes : ${
+          _(t)
+            .map(d =>
+              _(d)
+                .map()
+                .value()
+            )
+            .flattenDeep()
+            .value().length
+        } items.`
+      );
       await helpers.writeFileInFolder(
         `${config.pathData.local}${progDirectoryName}`,
         "",
@@ -107,7 +140,6 @@ try {
       );
     }
     database.detach(db);
-
   } catch (e) {
     console.log(e);
   }
